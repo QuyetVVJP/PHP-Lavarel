db30d19c7fea7f6ba82a2ea26510319e83385fae
QuyetVVJP <quyetvv12795@gmail.com>
2022-07-15 20:49:48 +0900

    Bai 3 cu phap can ban PHP

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+
+$a = 0;
+
+echo "QuyetVV";
+var_dump($a);
+$a = $a +1;
+echo $a;
+die(); //exit()
+// comment 1 dong
+# comment 1 dong
+/*
+    comment nhieu dong
+ *  */
+
+/*
+    Lenh echo -> render la noi dung chuoi
+ * var_dump("Quyet") -> in ra man hinh chuoi va kieu bien
+ * Lenh die() -> dung de debug check loi
+ *  */
