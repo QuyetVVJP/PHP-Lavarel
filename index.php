928a95ad8b53b0e6fa53c318bbf897893d4cd7d6
QuyetVVJP <quyetvv12795@gmail.com>
2022-07-15 21:50:45 +0900

    Bai 8 if else switch case

--- index.php
+++ index.php
@@ -2,21 +2,19 @@
 
 /*
 
- * Mảng tuần tự: Truy xuất phần tủ theo index
- * Mảng không tuần tự: index không nhất thiết phải là kiểu int, có thể là chữ, truy xuất phần tử theo key
- * Mảng đa chiều: Giống như json
+Câu điều kiện if else switch case
  *  */
 
-//$a = [1,2,3];
-//echo $a[2]; // 3
+$n = 1;
 
-$a = [
-    "key1" => 1,
-    "key2" => 2,
-    "key4" =>[
-        1,
-        "item1" => "ABC"
-    ]
-];
-$a["key3"] = 3;
-var_dump($a);
+switch ($n){
+    case 1:
+        echo 'Hello';
+        break;
+    case 2:
+        echo 'hi';
+        break;      
+    default :
+        echo " khong co gia tri phu hop";
+        break;
+}
