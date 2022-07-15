f7af9ab956a6388f2000a52bbd455c7611499746
QuyetVVJP <quyetvv12795@gmail.com>
2022-07-15 22:01:39 +0900

    Bai 9 Function

--- index.php
+++ index.php
@@ -2,19 +2,46 @@
 
 /*
 
-Câu điều kiện if else switch case
+Function()
+ * co gia tri tra ve
+ * khong co gia tri tra ve
+ *
+ Require, include: Thêm thư viện
+ * Require: sau khi chen file neu co loi xay ra thi se xuat ra thong bao va khong chay tiep nua
+ * Include: sau khi chen file neu co loi xay ra thi chương trình van tiep tuc chay 
  *  */
 
-$n = 1;
-
-switch ($n){
-    case 1:
-        echo 'Hello';
-        break;
-    case 2:
-        echo 'hi';
-        break;      
-    default :
-        echo " khong co gia tri phu hop";
-        break;
+function showName(){
+    echo "QUyet";
 }
+
+showName();
+
+
+function getName(){
+    return "Quyet";
+}
+
+var_dump(getName());
+
+// Tham trị
+function showValue($value){
+    echo $value."<br>";
+}
+
+showValue("Test value");
+
+
+// Tham chiếu: truyền vào địa chỉ bộ nhớ 
+
+function changeValue(&$value){
+    $value = 20;
+}
+
+$hi = 10;
+
+changeValue($hi);
+var_dump($hi);
+
+
+
