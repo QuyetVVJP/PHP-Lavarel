3af7ec280b8df84a1be1e45fe71ab5d675de6d30
QuyetVVJP <quyetvv12795@gmail.com>
2022-07-16 21:32:42 +0900

    bai 15 16 Session Cookie

--- index.php
+++ index.php
@@ -3,5 +3,18 @@
  Bản chất của POST GET là mảng không tuần tự
  POST: Dữ liệu gửi bởi POST thông qua HTTP Header, sử dụng $_POST để lấy dữ liệu
  GET: su dung $_GET để lấy dữ liệu
- *  */
 
+Cookie: luu thong tin o client
+Session: luu o server
+
+Cookie gui PHP_Session_ID len theo request, Web server detect PHP_Session_ID de phat hien session
+ *  */
+// setcookie(name, value, expire,...)
+//setcookie("quyet",1223123, 30*3600*24);
+//var_dump($_COOKIE);
+
+session_start();
+//$_SESSION["key"] = "Quyet";
+var_dump($_SESSION["key"]);die();
+
+unset($_SESSION["key"]); // xoa session
