e93e279b7b806fd3cce9246be5c021fc3c160efd
QuyetVVJP <quyetvv12795@gmail.com>
2022-08-01 17:14:05 +0900

    update about

--- Laravel_Tutorial/resources/views/clients/about.blade.php
+++ Laravel_Tutorial/resources/views/clients/about.blade.php
@@ -12,6 +12,8 @@
                 <li>Quê quán: Quảng Ninh, Việt Nam</li>
                 <li>Đại học: Bách Khoa Hà Nội - Khoa Điện Tử Viễn Thông</li>
                 <li>Hiện tại: Đang là học sinh trường tiếng tại Tokyo - Nhật Bản</li>
+                <li>SĐT: 070-2216-8476</li>
+                <li>Email: quyeta2ubqn@gmail.com</li>
                 </ul>
         </span>
         <hr>
@@ -20,30 +22,32 @@
             <ul >
                 <li>
                     <h5>FPT Software : Tester</h5>
-                   <span>- Lập trình và kiểm thử module SPI, CAN theo chuẩn AUTOSAR </span> <br>
+                    <h7>2018/03 - 2019/03</h7><br>
+                    <span>- Lập trình và kiểm thử module SPI, CAN theo chuẩn AUTOSAR </span> <br>
                     <span>- Ngôn ngữ và công cụ sử dụng: C, Ebtresos, Jira Git</span> <br>
                     <span>- Môi trường phát triển: Window</span>
                 </li>
                 <li>
                     <h5>DFT Software : Developer</h5>
+                    <h7>2021/03 - 2022/03</h7><br>
                     <ul style="list-style-type: circle;">
                         <li>
                             <h6>Hệ thống ERP</h6>
                             <span>- Tham gia phát triển hệ thống ERP cho doanh nghiệp, phát triển module quản lý lương cho phòng ban.
                             </span> <br>
-                            <span>- Ngôn ngữ và công cụ sử dụng: C#, Angular, MSSQL server, Gitlab</span> <br>
+                            <span>- Ngôn ngữ và công cụ sử dụng: ASP Framework, Angular, MSSQL server, Gitlab</span> <br>
                             <span>- Môi trường phát triển: Window</span>
                         </li>
                         <li>
                             <h6>Hệ thống IoT</h6>
-                             <span>- Tham gia phát triển dự án IoT Nông nghiệp, phát triển module báo cáo, quản trị (quản lý người dụng, vai trò, xây dựng các biểu đồ về dữ liệu cảm biến)
+                             <span>- Tham gia phát triển dự án IoT Nông nghiệp, phát triển module báo cáo, người dùng
                             </span> <br>
                             <span>- Ngôn ngữ và công cụ sử dụng: Spring boot, Angular, Postgresql, Gitlab</span><br>
                             <span>- Môi trường phát triển: Window</span>
                         </li>
                         <li>
                             <h6>Phần mềm quản lý tài nguyên máy ảo</h6>
-                            <span>- Phát triển Microservice thực hiện việc quản lý, theo dõi các máy ảo.
+                            <span>- Nhận outsource code từ phía khách hàng, phát triển phần mềm quản lý tài nguyên máy ảo với vCenter
                             </span> <br>
                             <span>- Ngôn ngữ và công cụ sử dụng: Spring boot, Angular, .Net, Postgresql, Ansible, Vcenter </span> <br>
                             <span>- Môi trường phát triển: Linux</span>
@@ -53,8 +57,9 @@
                 </li>
                 <li>
                     <h5>Blog - Mainichi shinpo</h5>
-                   <span>- Blog này được mình xây dựng khi được 1 công ty yêu cầu học ngôn ngữ mới </span> <br>
-                    <span>- Ngôn ngữ và công cụ sử dụng: PHP, Mysql, AWS</span> <br>
+                    <h7>2022/07</h7><br>
+                   <span>- Tự làm blog chia sẻ kinh nghiệm IT và quá trình học tiếng nhật của bản thân </span> <br>
+                    <span>- Ngôn ngữ và công cụ sử dụng: Laravel, Mysql, AWS</span> <br>
                     <span>- Môi trường phát triển: Window, Linux</span>
                 </li>
             </ul>
@@ -62,8 +67,8 @@
         <hr>
         <span><h4>Khóa học đã tham gia</h4></span>
         <ul>
-            <li>ITの日本語</li>
-            <li>AWS</li>
+            <li>ITの日本語: Quy trình phát triển phần mềm, Waterfall, Agile, Basic Design, Detail Design...</li>
+            <li>AWS: EC2, S3, VPC, IAM, Database, Route53...</li>
         </ul>
     </div>
 @endsection
